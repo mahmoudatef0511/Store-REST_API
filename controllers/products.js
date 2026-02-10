df1fb8561b5d5276e302caa264319146f3796a5a
mahmoudatef0511 <mahmoudatef0511@gmail.com>
2026-02-10 23:59:20 +0200

    Get all products controller modifications

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,64 @@
 const Product = require("../models/product");
 
-exports.getAllProducts = async (req, res) => {
+exports.getAllProductsStatic = async (req, res) => {
   try {
-    const products = await Product.find();
+    const products = await Product.find({ price: { $gt: 50 } })
+      .sort("price")
+      .select("name price");
     res.status(200).json({ products });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+};
+exports.getAllProducts = async (req, res) => {
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`,
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: +value };
+      }
+    });
+  }
+  try {
+    let query = Product.find(queryObject);
+    if (sort) {
+      query = query.sort(sort.split(",").join(" "));
+    } else {
+      query = query.sort("createdAt");
+    }
+    if (fields) {
+      query = query.select(fields.split(",").join(" "));
+    }
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const offset = (page - 1) * limit;
+    query = query.limit(limit).skip(offset);
+    const products = await query;
+    res.status(200).json({ products, count: products.length });
   } catch (err) {
     res.status(500).json({ msg: err });
   }
@@ -12,7 +67,7 @@ exports.postProduct = async (req, res) => {
   try {
     console.log(req.body);
     const product = await Product.create(req.body, {
-        runValidators: true
+      runValidators: true,
     });
     res.status(201).json({ msg: "Product created successfully!", product });
   } catch (err) {
@@ -36,7 +91,7 @@ exports.updateProduct = async (req, res) => {
     const { id: productID } = req.params;
     const product = await Product.findByIdAndUpdate(productID, req.body, {
       new: true,
-      runValidators: true
+      runValidators: true,
     });
     if (!product) {
       return res.status(404).json({ msg: `No product with ID ${productID} !` });
--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/products')
 
+router.get('/static', productsController.getAllProductsStatic);
 router.get('/', productsController.getAllProducts);
 router.post('/', productsController.postProduct);
 router.get('/:id', productsController.getProduct);
